de926f952a157d579084943f3c30328ba7bbe524
siddarth kotthur <kotthursiddarth@gmail.com>
2023-08-03 15:17:20 -0500

    kms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '$lib/app.css';
+</script>
+
+<div>
+	<!-- Navbar -->
+	<div>Navbar</div>
+	<!-- Background and name -->
+	<div>
+		<div
+			class="w-1/3 h-80 border-2 border-slate-300 pattern-boxes pattern-slate-300 pattern-bg-white dark:pattern-bg-black pattern-opacity-40 pattern-size-4"
+		>
+			<div class="">
+				<h1 class="text-6xl font-bold text-slate-50">Hi, Im Sid</h1>
+			</div>
+		</div>
+		<div>Links</div>
+	</div>
+</div>
